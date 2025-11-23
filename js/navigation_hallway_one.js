be740f609dc879c44c2c601ca6af3cbc49e567f9
malinik <malinik@umich.edu>
2025-11-23 10:45:00 -0500

    detailed cat path

--- js/navigation_hallway_one.js
+++ js/navigation_hallway_one.js
@@ -1,5 +1,6 @@
 let hallway = document.querySelector("h1");
 let content = document.querySelectorAll("p");
+let buttons = document.getElementById("buttons");
 hallway.addEventListener("click", function(){
     hallway.innerHTML = "Hallway (Stomach)";
     content[0].innerHTML = "This stomach lining is more colorful than expected."
@@ -9,13 +10,17 @@ hallway.addEventListener("dblclick", function(){
     content[0].innerHTML = "A cat-creature(?) appears.";
     content[1].innerHTML = "It purrs enticingly. It might like being pet."
     let img = document.getElementById("notebook");
-    img.innerHTML = "<img id='cat' src='images/cat.png' alt='Cat going right.' tabindex='0'>";
-    let buttons = document.getElementById("buttons");
-    buttons.innerHTML = '<p><a class= "btn" href="left_two.html">GO LEFT</a><a class= "btn" href="right_two.html">GO RIGHT</a></p>';
+    img.innerHTML = "<img id='cat' src='images/cat.png' alt='Cat-creature going right.' tabindex='0'>";
+    buttons.innerHTML = '<p><a class= "btn" href="left_two.html">FOLLOW CAT</a></p>';
+    let cat = document.getElementById("cat");
+    cat.addEventListener("click", () => move_cat());
 })
 
-let cat = document.getElementById("notebook");
-cat.addEventListener("dblclick", function(){
-    content[0].innerHTML = '"Wash my belly," says the cat.'
-    content[1].innerHTML = 'How does it access social media from here?<br><br>You pat down yourself to see if you have your phone so you can ask for the WiFi password or maybe switch to it\'s data plan. Sadly, your pockets are as empty as they were when you first woke up.';
-})
+function move_cat(){
+    let cat = document.getElementById("cat");
+    cat.classList.add("moveRight");
+    hallway.innerHTML = "Cat-Creature (Cute)";
+    content[0].innerHTML = '"Wash my belly," says the cat-creature.'
+    content[1].innerHTML = "How does it access social media from here?<br><br>You pat down yourself to see if you have your phone so you can ask for the WiFi password or maybe switch to it's data plan. Sadly, your pockets are as empty as they were when you first woke up.";
+    buttons.innerHTML = '<p><a class= "btn" href="left_two.html">FOLLOW CAT</a><a class= "btn" href="right_two.html">FOLLOW CAT</a></p>';
+}
